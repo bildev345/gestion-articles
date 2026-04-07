b16fc495b02ea8b58c208fa8a2630806c08bcd4c
Bilal el <bilalowraiylegh@gmail.com>
2026-04-07 13:01:29 +0100

    supprimer la duplication

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::get('devis/{devis}/duplicate', [DevisController::class, 'duplicate'])->na
 Route::get('/devis/restes', [DevisController::class, 'situation'])->name('devis.situation');
 /* ================== PAGES ================== */
 Route::get('/stock', [StockController::class, 'index'])->name('stock.index');
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+//Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
